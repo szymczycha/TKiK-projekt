b3a23a95074da387d9905cc1f05b4a16cfd52d89
Szymon Konieczny <skonieczny266@gmail.com>
2026-05-17 12:05:49 +0200

    tests run on all pascal code examples

--- test.py
+++ test.py
@@ -4,22 +4,23 @@ from antlr.pascalParser import pascalParser
 from antlr.pascalVisitor import pascalVisitor
 from helper.functions import print_tree
 
-filename = f"pascal_code/hello_world.pas"
-# filename = "pascal_code/9.pas"
-print(filename)
+for i in range(1, 18):
+    # filename = f"pascal_code/hello_world.pas"
+    filename = f"pascal_code/{i}.pas"
+    print(filename)
 
-input_stream = FileStream(filename)
+    input_stream = FileStream(filename)
 
-lexer = pascalLexer(input_stream)
-stream = CommonTokenStream(lexer)
+    lexer = pascalLexer(input_stream)
+    stream = CommonTokenStream(lexer)
 
-parser = pascalParser(stream)
+    parser = pascalParser(stream)
 
-tree = parser.program()
+    tree = parser.program()
 
-out_filename = "test.c"
-with open(out_filename, "w") as out_file:
-    visitor = pascalVisitor(out_file)
-    tree.accept(visitor)
-    
-print(tree.toStringTree(recog=parser))
+    out_filename = f"compiled_pascal_to_c/{i}.c"
+    with open(out_filename, "w") as out_file:
+        visitor = pascalVisitor(out_file)
+        tree.accept(visitor)
+        
+    print(tree.toStringTree(recog=parser))
