83899de56658aee9268f88aeb22ae8d3477e8bed
Szymon Konieczny <skonieczny266@gmail.com>
2026-05-28 12:12:48 +0200

    added example

---
+++ pascal_code/erastotenes_sieve.pas
@@ -0,0 +1,32 @@
+program EratosthenesSieve;
+
+var
+  isPrime: array[2..100] of boolean;
+  i, j: integer;
+
+begin
+  for i := 2 to 100 do
+    isPrime[i] := true;
+
+  for i := 2 to trunc(sqrt(100)) do
+  begin
+    if isPrime[i] then
+    begin
+      j := i * i;
+      while j <= 100 do
+      begin
+        isPrime[j] := false;
+        j := j + i;
+      end;
+    end;
+  end;
+
+  writeln('Prime numbers up to 100:');
+  for i := 2 to 100 do
+  begin
+    if isPrime[i] then
+      writeln(i, ' ');
+  end;
+
+  writeln;
+end.
