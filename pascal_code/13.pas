7e1b8cd84a5d0c95284c5f8f1ee6058fa82c073e
Szymon Konieczny <skonieczny266@gmail.com>
2026-05-28 11:11:33 +0200

    changed example to resemble modern pascal

--- pascal_code/13.pas
+++ pascal_code/13.pas
@@ -6,18 +6,12 @@ var
 function factorial(x: integer): integer;
 begin
   if x = 1 then
-    factorial := 1
+    result := 1
   else
-    factorial := x * factorial(x - 1);
-end;
-
-procedure inc(var x: integer);
-begin
-  x := x + 1;
+    result := x * factorial(x - 1);
 end;
 
 begin
   n := 5;
-  inc(n);
   result := factorial(n);
 end.
